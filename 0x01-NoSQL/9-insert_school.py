a2c650754afae81410fa0d1fad6d5b96d0684a0d
Maurice <maurice.nzai@gmail.com>
2024-03-26 08:50:41 +0000

    Add Task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+MongoDB Operations with Python using pymongo
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on kwargs
+    """
+    document_id = mongo_collection.insert_one(kwargs)
+    return document_id
