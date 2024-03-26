f611c76044083b924f71a8567c20baa8b824a23b
Maurice <maurice.nzai@gmail.com>
2024-03-26 18:17:11 +0000

    Update Task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -8,5 +8,5 @@ def insert_school(mongo_collection, **kwargs):
     """
     Inserts a new document in a collection based on kwargs
     """
-    document_id = mongo_collection.insert_one(kwargs)
+    document_id = mongo_collection.insert(kwargs)
     return document_id
